1a0a287acc1cc6c1b32160b76fdd0a5944698cd1
lijiankun24 <jiankunli24@gmail.com>
2018-11-27 08:48:52 +0800

    Add some codes about buildTypes.

--- app/src/main/java/com/lijiankun24/gradleforandroid/MainActivity.kt
+++ app/src/main/java/com/lijiankun24/gradleforandroid/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.lijiankun24.gradleforandroid
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.tv)
+        textView.text = Constants.BUILD_TYPE
     }
 }
---
+++ app/src/release/java/com/lijiankun24/gradleforandroid/Constants.java
@@ -0,0 +1,10 @@
+package com.lijiankun24.gradleforandroid;
+
+/**
+ * Constants.java
+ * <p>
+ * Created by lijiankun on 2018/11/27.
+ */
+public class Constants {
+    public static final String BUILD_TYPE = "release";
+}
